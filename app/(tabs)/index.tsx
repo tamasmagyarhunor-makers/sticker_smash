d50533ab4d8d77c5eae1dfa8acd02347b31475eb
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-03-24 05:26:41 +0000

    Add more features

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,34 @@
+import { View, StyleSheet } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceHolderImage = require('@/assets/images/background-image.png');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo"/>
+        <Button label="Use this photo"/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home nav'}}/>
-      <Stack.Screen name="about" options={{ title: 'About nav'}}/>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
